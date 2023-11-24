679df01d79eb3492fdc3c3e52b1e9e403dda7220
Yuki Asano <yasano@g.ecc.u-tokyo.ac.jp>
2023-11-24 17:24:14 +0900

    enable start/stop by ros topic

--- scene_demo/conveyor_belt/src/conveyor_belt/conveyor_belt.ino
+++ scene_demo/conveyor_belt/src/conveyor_belt/conveyor_belt.ino
@@ -7,6 +7,31 @@ bool detect_switch_flag = false;
 extern Adafruit_TCS34725 tcs;
 Ultrasonic ultrasonic(23);
 extern int i;
+
+// ros
+#include <ros.h>
+#include <std_msgs/Bool.h>
+ros::NodeHandle nh;
+std_msgs::Bool is_conveyor_on;
+ros::Publisher pub("is_conveyor_on", &is_conveyor_on);
+
+void start_cb(const std_msgs::Bool &msg){
+  if(msg.data){
+    set_speed(1, 200, NULL, false);  // modify direction from belt_move()
+    is_conveyor_on.data = true;
+  }
+}
+
+void stop_cb(const std_msgs::Bool &msg){
+  if(msg.data){
+    belt_stop();
+    is_conveyor_on.data = false;
+  }
+}
+
+ros::Subscriber<std_msgs::Bool> sub_start("start_conveyor", &start_cb);
+ros::Subscriber<std_msgs::Bool> sub_stop("stop_conveyor", &stop_cb);
+
 void setup() {
   Serial.begin(115200);
   Serial.println(SOFTWARE_VERSION);
@@ -25,10 +50,20 @@ void setup() {
   mode = NOTHING_MODE;
   delay(1000);
 
+   // ros
+  nh.getHardware()->setBaud(115200);
+  nh.initNode();
+  nh.subscribe(sub_start);
+  nh.subscribe(sub_stop);
+  nh.advertise(pub);
 }
 void loop() {
   first_page();
   //get_color();
   //conver_work();
-  belt_move();
+  //belt_move();
+
+  //ros
+  nh.spinOnce();
+  pub.publish(&is_conveyor_on);
 }
